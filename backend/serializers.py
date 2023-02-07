edfb3d69a6c6762836f0144303fcd14070748ada
Logiudice Manuel <51341719+logiudicemanuel@users.noreply.github.com>
2023-02-07 10:29:35 +0100

    Add files via upload

--- backend/serializers.py
+++ backend/serializers.py
@@ -1,13 +1,15 @@
 from rest_framework import serializers
 from .models import ViaggiFatti
 from .models import ViaggiDaFare
-
+from django.contrib.auth.models import User
+from rest_framework.permissions import IsAuthenticated
 class ViaggiFattiSerializers(serializers.ModelSerializer):
     class Meta:
         model = ViaggiFatti
-        fields = "__all__"
+        fields = ["id","Titolo", "Data", "Descrizione", "Luogo"]
 
 class ViaggiDaFareSerializers(serializers.ModelSerializer):
+
     class Meta:
         model = ViaggiDaFare
-        fields = "__all__"
+        fields = ["id","Titolo", "Data", "Attivita", "Luogo"]
--- progetto/urls.py
+++ progetto/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('backend.urls')),
     path('admin/', admin.site.urls),
+    path('api/v1/',include('djoser.urls')),
+    path('api/v1/',include('djoser.urls.authtoken'))
 ]
